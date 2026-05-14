1e252a9c617d595787dcccfe6efe4a8facde9c17
Eunice Hwang <eunicehwang06@gmail.com>
2026-05-14 13:34:58 -0400

    filling out israeli40s story

--- src/pages/Israeli40s.jsx
+++ src/pages/Israeli40s.jsx
@@ -15,11 +15,15 @@ function Israeli40s() {
     return (
         <div className="game-div">
             <Link to='/' className='page-title'>CORE 388: Final Project, Eunice Hwang</Link>
-            <div className='game-text'>
+            <div className='year'>{currentNode.year}</div>
+            <div className={currentNode.type == "fact" ? 'fact-text' : 'game-text'}>
                 <p>{currentNode.text.join(' ')}</p>
             </div>
             <div className="game-choices">
-                {currentNode.type == "end" ? <Link to='/pick-time' className='button'>Try a different perspective.</Link> : currentNode.choices.map(choice => <button className="button" key={choice.label} onClick={() => handleChoice(choice)}>{choice.label}</button>)}
+                {currentNode.end ? <Link to='/pick-time' className='button'>Try another perspective.</Link> : currentNode.choices.map(choice => <button className="button" key={choice.label} onClick={() => handleChoice(choice)}>{choice.label}</button>)}
+            </div>
+            <div className='footnote'>
+                {currentNode.footnotes ? currentNode.footnotes.map((note, index) => <p key={index}>{note}</p>) : <p></p>}
             </div>
         </div>
     )
