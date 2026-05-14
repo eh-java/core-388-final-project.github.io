fd869b31e6c5bf879d92d91b496618e0ca7d6efe
Eunice Hwang <eunicehwang06@gmail.com>
2026-05-13 20:43:44 -0400

    created Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,20 @@
+import '../App.css';
+
 // home page for website
 function Home() {
     return (
-        <button>Hello!</button>
+        <div className="parent-div">
+            <p className='page-title'>CORE 388: Final Project, Eunice Hwang</p>
+            <div className='title'>
+                <p>WHAT IF IT WERE YOU?</p>
+                <p className='sub-title'>Exploring Israeli and Palestinian Narratives</p>
+            </div>
+            <div className="default">
+                <button className="button">PLAY</button>
+                <p></p>
+                <button className="button">SOURCES</button>
+            </div>
+        </div>
     )
 }
 
